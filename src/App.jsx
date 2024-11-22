73d3c9fa98c7b4eaae1ca8f2cb123de9d0c8ca33
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-22 11:47:46 +0000

    feat: fetching data to the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from 'react'
 
-import moviesResults from './mocks/movies-results.json'
-
 import './App.scss'
 
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 
+const OMDB_API_URL_PREFIX = 'http://www.omdbapi.com/?apikey='
+const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY
+
 const App = () => {
   const [movies, setMovies] = useState([])
 
   const getMovies = ({ search }) => {
-    console.log('getMovies by => ', search)
+    fetch(`${OMDB_API_URL_PREFIX}${OMDB_API_KEY}&s=${search}`)
+      .then((response) => {
+        if (!response.ok) throw new Error('Failed to fetch movies search')
 
-    const newMovies = moviesResults.Search
-
-    setMovies(newMovies)
+        return response.json()
+      }).then((data) => {
+        const newMovies = data.Search
+        setMovies(newMovies)
+      }).catch((error) => {
+        console.error(error.message)
+      })
   }
 
   return (
