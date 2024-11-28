a0d712fd464e5ac186b8bb2f0a2ccbbbdbfc27f8
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-28 11:48:30 +0000

    fix: update checkbox for iOS compatibility style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,7 +47,36 @@ const Header = ({ getMovies, checkSortMovies }) => {
         <p className='sort-movies-wrapper__text'>Do you want to sort the movies by title?</p>
         <div className='checkbox-wrapper'>
           <input type='checkbox' id='sort-movies' className='checkbox-wrapper__checkbox' checked={sort} onChange={handleSortMoviesChange} />
-          <label htmlFor='sort-movies' className='checkbox-wrapper__label' />
+          <label htmlFor='sort-movies' className='checkbox-wrapper__label'>
+            <span className='checkbox-checkmark'>
+              <svg
+                width='800px'
+                height='800px'
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+                <g
+                  id='SVGRepo_tracerCarrier'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+                <g id='SVGRepo_iconCarrier'>
+                  <g id='Interface / Check'>
+                    <path
+                      id='Vector'
+                      d='M6 12L10.2426 16.2426L18.727 7.75732'
+                      stroke='#fcc434'
+                      strokeWidth='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                  </g>
+                </g>
+              </svg>
+            </span>
+          </label>
         </div>
       </div>
     </header>
